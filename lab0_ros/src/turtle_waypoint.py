76139285f0b175d73854d3e1dd4c5097b3d71e07
student <student@ubuntu-VirtualBox.(none)>
2016-02-18 08:34:06 +0100

    lab0: corrected to spaces

--- lab0_ros/src/turtle_waypoint.py
+++ lab0_ros/src/turtle_waypoint.py
@@ -22,62 +22,62 @@ tolerance=0.1
 gotPosition=False
 
 def callback(pose_msg):
-	global x,y,theta,gotPosition
-	#TODO:Store the position in x,y and theta variables.
-	##
-	##
-	##
-	##
-	##
-	gotPosition=True
+    global x,y,theta,gotPosition
+    #TODO:Store the position in x,y and theta variables.
+    ##
+    ##
+    ##
+    ##
+    ##
+    gotPosition=True
 
 def waypoint():
-	global gotPosition
-	#TODO: Define the pulisher: Name of the topic. Type of message
-	#
+    global gotPosition
+    #TODO: Define the pulisher: Name of the topic. Type of message
+    #
 
-	#Name of the node
-	rospy.init_node('turtle_waypoint')
-	#TODO: Define the subscriber: Name of the topic. Type of message. Callback function
-	#
+    #Name of the node
+    rospy.init_node('turtle_waypoint')
+    #TODO: Define the subscriber: Name of the topic. Type of message. Callback function
+    #
 
-	#Has the turtle reach position?
-	finished=False
-	#If the point hasn't been specified in a command line:
-	if(len(sys.argv)!=3):
-		print('X and Y values not set or not passed correctly. Looking for default parameters.')
-		#TODO: If ROS parameters default_x and default_y exist:
-		if True: #Change this for the correct expression
-			#TODO: Save them into this variables
-			x_desired=0 #Change this for the correct expression
-			y_desired=0 #Change this for the correct expression
-			print('Heading to: %f,%f' %(x_desired, y_desired))
-		else:
-			print('Default values parameters not set!. Not moving at all')
-			finished=true
-	else:
-		#Save the command line arguments.
-		x_desired=float(sys.argv[1])
-		y_desired=float(sys.argv[2])
-		print('Heading to: %f,%f' %(x_desired, y_desired))
+    #Has the turtle reach position?
+    finished=False
+    #If the point hasn't been specified in a command line:
+    if(len(sys.argv)!=3):
+        print('X and Y values not set or not passed correctly. Looking for default parameters.')
+        #TODO: If ROS parameters default_x and default_y exist:
+        if True: #Change this for the correct expression
+            #TODO: Save them into this variables
+            x_desired=0 #Change this for the correct expression
+            y_desired=0 #Change this for the correct expression
+            print('Heading to: %f,%f' %(x_desired, y_desired))
+        else:
+            print('Default values parameters not set!. Not moving at all')
+            finished=true
+    else:
+        #Save the command line arguments.
+        x_desired=float(sys.argv[1])
+        y_desired=float(sys.argv[2])
+        print('Heading to: %f,%f' %(x_desired, y_desired))
 
-	while not rospy.is_shutdown() and not finished:
-		if(gotPosition):
-			#TODO: Send a velocity command for every loop until the position is reached within the tolerance.
-			#
-			#
-			#
-			#
-			#
-			#
-			#
-			#
-			#
-			if True: #Change for the correct expression
-				finished=True		
+    while not rospy.is_shutdown() and not finished:
+        if(gotPosition):
+            #TODO: Send a velocity command for every loop until the position is reached within the tolerance.
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            if True: #Change for the correct expression
+                finished=True        
 
-		#Publish velocity commands every 0.3 sec.
-		rospy.sleep(0.3)
+        #Publish velocity commands every 0.3 sec.
+        rospy.sleep(0.3)
 
 if __name__ == '__main__':
     try:
